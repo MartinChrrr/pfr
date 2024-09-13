2e60b67e025588434e3898337a3a7c48ff542c63
MartinChrrr <martin_charrier@yahoo.fr>
2024-09-13 16:09:40 +0200

    Fin no.php

--- no.php
+++ no.php
@@ -19,9 +19,15 @@ if ($_SESSION['id'] != null && $_SESSION['id'] != "" && $_SESSION['nom_utilisate
     $sql_import_matching = "SELECT * FROM matching WHERE (user_id1 = '$id' AND user_id2 = '$other_id') OR (user_id2 = '$id' AND user_id1 = '$other_id')";
     $verify_match = $connexion->query($sql_import_matching);
     if($verify_match->num_rows > 0) {
-        echo "nulm";
+        while($row = $verify_match->fetch_assoc()) {
+            if($id == $row['user_id2']) {
+                $connexion->query("UPDATE matching SET like_user2 = 0");
+                header("Location: match.php");
+
+            }
+        }
     } else {
-        $sql_create = "INSERT INTO matching (user_id1, user_id2, like_user1) VALUES ('$id', '$other_id', 1)";
+        $sql_create = "INSERT INTO matching (user_id1, user_id2, like_user1) VALUES ('$id', '$other_id', 0)";
         if($connexion->query($sql_create) === TRUE){
             header("Location: match.php");
         }
